118e6ae7b56ed14b0ab0e9ebf472516952a5a2be
Delabeneta <crazayfrog@gmail.com>
2025-06-08 19:11:37 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
 const app = new Hono();
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
-});
 
 serve({ fetch: app.fetch, port: 3000 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`);
